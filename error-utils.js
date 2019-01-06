e1780cecb83eadfcdd1e1b7d12a5a4cd14168aa2
Rajiv Narayana Singaseni <rajivnarayana@gmail.com>
2019-01-07 00:08:53 +0530

    Move sources into src folder

--- error-utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function processMongooseErrors(error) {
-    return Object.keys(error.errors).map(key => {
-        return { message: error.errors[key] };
-    });
-}
-exports.processMongooseErrors = processMongooseErrors;
-//# sourceMappingURL=error-utils.js.map
